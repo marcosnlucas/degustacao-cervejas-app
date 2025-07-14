cf3975afe8cae664160cc63a78e1744eef16b087
marcosnlucas <marcosnuneslucas@MacBook-Air-de-Arklok-4.local>
2025-07-14 15:23:15 -0300

    refactor: simplify NextAuth config to use default database session strategy

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -11,32 +11,8 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  session: {
-    strategy: 'jwt',
-  },
   pages: {
     signIn: '/login',
-  },
-  callbacks: {
-    async session({ session, token }) {
-      if (token && session.user) {
-        session.user.id = token.id as string;
-        session.user.name = token.name;
-        session.user.email = token.email;
-        session.user.image = token.picture;
-      }
-      return session;
-    },
-    async jwt({ token, user }) {
-      if (user) {
-        // Na primeira vez que o JWT é criado, 'user' está disponível
-        token.id = user.id;
-        token.name = user.name;
-        token.email = user.email;
-        token.picture = user.image;
-      }
-      return token;
-    },
   },
   secret: process.env.NEXTAUTH_SECRET,
 };
