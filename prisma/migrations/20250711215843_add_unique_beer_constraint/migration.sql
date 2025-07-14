e406433de42482dba2eff1c20193251d30561b20
marcosnlucas <marcosnuneslucas@MacBook-Air-de-Arklok-4.local>
2025-07-14 12:20:55 -0300

    feat: Reset migrations for PostgreSQL compatibility

--- prisma/migrations/20250711215843_add_unique_beer_constraint/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[name,breweryId]` on the table `Beer` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Beer_name_breweryId_key" ON "Beer"("name", "breweryId");
