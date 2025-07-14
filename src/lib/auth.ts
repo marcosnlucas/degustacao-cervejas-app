0ecd435939dec3ecd1e7e7e389d877f70d89940c
marcosnlucas <marcosnuneslucas@MacBook-Air-de-Arklok-4.local>
2025-07-14 17:52:11 -0300

    feat: include user id in session to authorize data fetching

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,6 +5,14 @@ import { prisma } from '@/lib/prisma';
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
+  callbacks: {
+    async session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
